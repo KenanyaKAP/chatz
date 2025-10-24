2e15c334d7d65c8c553738f01889d13970cbe88c
KenanyaKAP <kenanyaline@gmail.com>
2025-10-24 21:14:07 +0700

    feat: Add react render

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from React!</h1>
+      <p>
+        This is a React component rendered in a Rails application using Vite.
+      </p>
+    </div>
+  );
+};
+
+export default App;
